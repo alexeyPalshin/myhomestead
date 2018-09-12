b83d10badb7c53d08155edf6e09a22500b55f59b
Alexey Palshin <a.palshin@belitsoft.com>
2018-09-12 18:11:38 +0300

    mysql added

--- provision/docker.sh
+++ provision/docker.sh
@@ -13,7 +13,7 @@ sudo add-apt-repository \
  stable"
 sudo apt-get update
 sudo apt-get -y install docker-ce
-sudo usermod -aG docker ubuntu
+sudo usermod -aG docker $USER
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
